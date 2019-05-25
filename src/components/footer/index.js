ed159ea7145e4caa87771744610abcfabd25b898
Wuzuofu1996 <1574907674@qq.com>
2019-05-25 23:54:18 +0800

    react

---
+++ src/components/footer/index.js
@@ -0,0 +1,25 @@
+﻿import React, { Component } from 'react'
+
+import './index.css'
+
+
+
+class Footer extends Component {
+
+    clear = () => {
+
+        this.props.clear()
+
+    }
+
+    render() {
+
+        return (<div className="footer">Copyright © 2019 wuzuofu <span className="clear-btn" onClick={this.clear}>clear</span></div>)
+
+    }
+
+}
+
+
+
+export default Footer
---
+++ src/components/header/search/index.js
@@ -0,0 +1,29 @@
+﻿import React, { Component } from 'react'
+
+import './index.css'
+
+class Search extends Component {
+
+    enterPress = (event) => {
+
+        if(event.key === 'Enter') {
+
+            this.props.enterPress(event.target.value)
+
+            event.target.value = ''
+
+        }
+
+    }
+
+    render() {
+
+        return (<input type="text" onKeyPress={this.enterPress} className="search" placeholder="添加ToDo" required="required" autoComplete="off"></input>)
+
+    }
+
+}
+
+
+
+export default Search
